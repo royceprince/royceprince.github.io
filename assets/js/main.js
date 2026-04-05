5816835b7df28affb63353a031ea351fd75d632d
princechaurasia023 <princechaurasia023@gmail.com>
2026-04-05 19:59:19 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -435,3 +435,27 @@ document.documentElement.setAttribute("data-theme","dark");
 }
 });
 }
+   let time = 1500;
+let timer;
+
+function startTimer(){
+timer = setInterval(()=>{
+time--;
+let minutes = Math.floor(time/60);
+let seconds = time%60;
+
+document.getElementById("timer").innerHTML =
+minutes + ":" + (seconds<10?"0":"") + seconds;
+
+if(time<=0){
+clearInterval(timer);
+}
+},1000);
+}
+
+function resetTimer(){
+clearInterval(timer);
+time = 1500;
+document.getElementById("timer").innerHTML="25:00";
+}
+   
